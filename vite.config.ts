c2ce4f25276ef23dbb78479f3c31fd0c088c2bb4
Arvinjun Nie <niezicheng@shein.com>
2024-06-07 15:54:37 +0800

    build: basicSsl、logger、define and log clear

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, createLogger } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from  'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+const isProd = ['prod', 'production'].includes(process.env.NODE_ENV);
+
+const logger = createLogger();
+const originalWarning = logger.warn;
+logger.warn = (msg, options) => {
+  if (msg.includes('Default and named imports from CSS files are deprecated. ')) return;
+  originalWarning(msg, options);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  esbuild: {
+    drop: isProd ? ['debugger', 'console'] : []
+  },
+  customLogger: logger,
+  define: {
+    'process.env': {},
+    'Buffer.from': {},
+    global: 'window'
+  },
+  plugins: [react(), tsconfigPaths(), basicSsl()],
   server: {
     host: '127.0.0.1',
     port: 5173,
@@ -20,10 +39,7 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ]
+      plugins: [tailwindcss, autoprefixer]
     }
   }
 });
